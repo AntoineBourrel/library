f7013f7c87d7cbfdc0be0fafd1e282b7ddfa9f8a
Antoine BOURREL <94041592+AntoineBourrel@users.noreply.github.com>
2022-07-21 17:22:30 +0200

    ajout nightmode

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,17 @@
 const nightToggleBtn = document.querySelector('.js-btn-night');
+const body = document.querySelector('body');
 
+if (localStorage.getItem('isNightMode') === 'true') {
+    body.classList.add('night-template');
+}
 
 nightToggleBtn.addEventListener('click', function(){
-    const body = document.querySelector('body');
-    body.classList.add("night-template");
+    body.classList.toggle("night-template");
+    if(body.className != "night-template" || localStorage.getItem('isNightMode') === 'false'){
+        nightToggleBtn.innerText = "Night Mode";
+        localStorage.setItem('isNightMode', false);
+    } else if(body.className == "night-template" || localStorage.getItem('isNightMode') === 'true') {
+        nightToggleBtn.innerText = "light Mode";
+        localStorage.setItem('isNightMode', true);
+    }
 });
