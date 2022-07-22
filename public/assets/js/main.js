c42a61c88cb7f72cfd17bc4ce1eaa6684fdcb233
Antoine BOURREL <94041592+AntoineBourrel@users.noreply.github.com>
2022-07-22 14:12:23 +0200

    menu burger

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,23 +1,38 @@
 //création de sélecteur
-const nightToggleBtn = document.querySelector('.js-btn-night');
+const nightToggleBtn = document.querySelectorAll('.js-btn-night');
 const body = document.querySelector('body');
+const icon = document.querySelector('.icons');
+const menu = document.querySelector('.menu-mobile');
 
-// vérification si le night mode est activé en arrivant sur une page
+
+//vérification si le night mode est activé en arrivant sur une page
 if (localStorage.getItem('isNightMode') === 'true') {
     body.classList.add('night-template');
-    nightToggleBtn.innerText = "light Mode";
+    nightToggleBtn.forEach(function(btn){
+        btn.innerText = 'Day Mode';
+    });
 }
 
-// Listener sur le bouton NightMode et vérification si le NightMode est activé ou non
-nightToggleBtn.addEventListener('click', function(){
-    // Ajout de la class "night-template" au 'body' de la page html
-    body.classList.toggle("night-template");
-    // Stockage dans le localStorage
-    localStorage.setItem('isNightMode', true);
-    if(body.className != "night-template" || localStorage.getItem('isNightMode') === 'false'){
-        nightToggleBtn.innerText = "Night Mode";
-        localStorage.setItem('isNightMode', false);
-    } else if(body.className == "night-template" || localStorage.getItem('isNightMode') === 'true') {
-        nightToggleBtn.innerText = "Day Mode";
-    }
+
+nightToggleBtn.forEach(function (btn){
+    btn.addEventListener('click', function() {
+        // Ajout de la class "night-template" au 'body' de la page html
+        body.classList.toggle("night-template");
+        // Stockage dans le localStorage
+        localStorage.setItem('isNightMode', true);
+        if (body.className != "night-template" || localStorage.getItem('isNightMode') === 'false') {
+            btn.innerText = "Night Mode";
+            localStorage.setItem('isNightMode', false);
+        } else if (body.className == "night-template" || localStorage.getItem('isNightMode') === 'true') {
+            btn.innerText = "Day Mode";
+        }
+    })
+});
+
+
+
+
+icon.addEventListener('click', function(){
+    console.log('test');
+    menu.classList.toggle('d-b');
 });
